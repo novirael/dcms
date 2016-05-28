d432c7d3a7dbd46a7ad8a9e1aae7c3d2d58ec2ef
novirael <novirael@gmail.com>
2016-05-28 20:51:00 +0200

    Change db to mysql, create requirements

--- dcms/settings.py
+++ dcms/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'dcms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dcmsdb',
+        'USER': 'dcmsuser',
+        'PASSWORD': 'dcmspswd',
+        'HOST': 'localhost',
     }
 }
 
