cbab089924e8bcd18a1810e56565cdd7e9331c4f
novirael <novirael@gmail.com>
2016-06-03 21:42:39 +0200

    Extend custom user model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
@@ -5,4 +6,23 @@ from authentication.models import CustomUser
 
 admin.site.site_header = 'DCMS administration'
 
-admin.site.register(CustomUser, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': (
+            'first_name', 'last_name', 'email', 'pesel', 'date_of_birth',
+        )}),
+        (_('Contact details'), {'fields': (
+            'city', 'post_code', 'address', 'telephone',
+        )}),
+        (_('Medical details'), {'fields': ('is_doctor', 'medical_right_number',
+        )}),
+        (_('Permissions'), {'fields': (
+            'is_active', 'is_staff', 'is_superuser', 'groups',
+        )}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- authentication/models.py
+++ authentication/models.py
@@ -6,7 +6,22 @@ from visits.models import PatientCard
 
 class CustomUser(AbstractUser):
     is_doctor = models.BooleanField(default=False)
-    medical_right_number = models.CharField(max_length=32)
+    medical_right_number = models.CharField(max_length=16, blank=True)
+
+    pesel = models.CharField(max_length=11, blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    city = models.CharField(max_length=32, blank=True)
+    post_code = models.CharField(max_length=32, blank=True)
+    address = models.CharField(max_length=64, blank=True)
+    telephone = models.CharField(max_length=12, blank=True)
 
     def __str__(self):
         return self.get_full_name()
+
+    def get_full_address(self):
+        return "{post_code} {city}, {address}".format(
+            post_code=self.post_code.strip(" "),
+            city=self.city.strip(" "),
+            address=self.address.strip(" ")
+        )
