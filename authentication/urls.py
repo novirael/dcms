8c32f03120eec8314fba34ca0dda7225200d19aa
novirael <novirael@gmail.com>
2016-06-04 00:51:34 +0200

    Add patient and doctor index

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,20 @@
 from django.conf.urls import url
 
-from authentication.views import PatientCardDetailView, PatientDetailView, DoctorDetailView
+from authentication.views import (
+    PatientCardDetailView,
+    PatientIndexView,
+    PatientDetailView,
+    DoctorIndexView,
+    DoctorDetailView,
+)
+
 
 urlpatterns = [
     url(r'^cards/(?P<pk>\d+)/$', PatientCardDetailView.as_view(), name='card_details'),
+
+    url(r'^doctors/$', DoctorIndexView.as_view(), name='doctor_index'),
     url(r'^doctors/(?P<pk>\d+)/$', DoctorDetailView.as_view(), name='doctor_details'),
-    url(r'^patients/(?P<pk>\d+)/$', PatientDetailView.as_view(), name='patients_details'),
+
+    url(r'^patients/$', PatientIndexView.as_view(), name='patient_index'),
+    url(r'^patients/(?P<pk>\d+)/$', PatientDetailView.as_view(), name='patient_details'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,21 @@
-from django.views.generic import DetailView
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, ListView
 
 from authentication.models import CustomUser
 from visits.models import PatientCard
+
+
+class UserIndexView(ListView):
+    template_name = 'authentication/index.html'
+    model = CustomUser
+    context_object_name = 'people'
+    paginate_by = 20
+    is_doctor = None
+
+    def get_context_data(self, **kwargs):
+        context = super(UserIndexView, self).get_context_data(**kwargs)
+        context['is_doctor'] = self.is_doctor
+        return context
 
 
 class PatientCardDetailView(DetailView):
@@ -10,12 +24,26 @@ class PatientCardDetailView(DetailView):
     context_object_name = 'card'
 
 
+class DoctorIndexView(UserIndexView):
+    is_doctor = True
+    queryset = CustomUser.objects.filter(
+        groups__name__in=["Doctor"]
+    )
+
+
 class DoctorDetailView(DetailView):
     template_name = 'authentication/doctor_details.html'
     model = CustomUser
     context_object_name = 'doctor'
     queryset = CustomUser.objects.filter(
         groups__name__in=["Doctor"]
+    )
+
+
+class PatientIndexView(UserIndexView):
+    is_doctor = False
+    queryset = CustomUser.objects.filter(
+        groups__name__in=["Patient"]
     )
 
 
