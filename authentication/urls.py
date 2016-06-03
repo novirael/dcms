ea96538900bd935b5d328e5f4229fbbdb24d2134
novirael <novirael@gmail.com>
2016-06-04 00:07:50 +0200

    Add patient and doctor details

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from authentication.views import PatientCardDetailView
-
+from authentication.views import PatientCardDetailView, PatientDetailView, DoctorDetailView
 
 urlpatterns = [
     url(r'^cards/(?P<pk>\d+)/$', PatientCardDetailView.as_view(), name='card_details'),
+    url(r'^doctors/(?P<pk>\d+)/$', DoctorDetailView.as_view(), name='doctor_details'),
+    url(r'^patients/(?P<pk>\d+)/$', PatientDetailView.as_view(), name='patients_details'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import DetailView
 
+from authentication.models import CustomUser
 from visits.models import PatientCard
 
 
@@ -7,3 +8,21 @@ class PatientCardDetailView(DetailView):
     template_name = 'authentication/card_details.html'
     model = PatientCard
     context_object_name = 'card'
+
+
+class DoctorDetailView(DetailView):
+    template_name = 'authentication/doctor_details.html'
+    model = CustomUser
+    context_object_name = 'doctor'
+    queryset = CustomUser.objects.filter(
+        groups__name__in=["Doctor"]
+    )
+
+
+class PatientDetailView(DetailView):
+    template_name = 'authentication/patient_details.html'
+    model = CustomUser
+    context_object_name = 'patient'
+    queryset = CustomUser.objects.filter(
+        groups__name__in=["Patient"]
+    )
